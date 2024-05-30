b94eae5cedfe1d04077f7c8a9d8b55275c3c68be
Lucas Lu <33080381+shichenglu@users.noreply.github.com>
2024-05-30 03:08:53 -0700

    Update FP-College-Info-LLM.py

--- FP-College-Info-LLM.py
+++ FP-College-Info-LLM.py
@@ -61,7 +61,7 @@ def stream_response():
 
 hide_streamlit_style = """
             <style>
-            .viewerBadge_container__r5tak {visibility: hidden !important; display:none !important;}
+            a[class^=viewerBadge] {visibility: hidden !important; display:none !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
